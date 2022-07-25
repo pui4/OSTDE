fdc655ea60fddf18293443b1561cc46333808698
Precious13ui (Daniel Pitman) <70605806+Precious13ui@users.noreply.github.com>
2022-07-25 18:27:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@ apt install xorg -y
 apt install kwin-x11 --no-install-recommends -y
 apt install plank -y
 
-apt install lightdm
+apt install lightdm -y
 sudo systemctl start lightdm.service
 
 chmod +x ostde.sh
 
 mv ostde.sh /bin/ostde.sh
 mv ostde.desktop /usr/share/xsessions/ostde.desktop
+
+sudo apt-get install -y gnome-shell-extension-dash-to-panel
