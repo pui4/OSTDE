284fbad823e77f04324611d94c5ba71e8292a4d4
Precious13ui (Daniel Pitman) <70605806+Precious13ui@users.noreply.github.com>
2022-07-25 18:05:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@ apt install xorg -y
 apt install kwin-x11 --no-install-recommends -y
 apt install plank -y
 
+apt install lightdm
+sudo systemctl start lightdm.service
+
 chmod +x ostde.sh
 
 mv ostde.sh /bin/ostde.sh
